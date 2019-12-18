b6c0e51d298d92eb3c5db5be96022554ec2bfc12
nagaraju <nagaraju275521@gmail.com>
2019-12-18 15:09:55 +0530

    Added new pojo student file

---
+++ MySchool/src/com/vk/Student.java
@@ -0,0 +1,29 @@
+package com.vk;
+
+public class Student {
+		
+	private String name;
+	private int age;
+	private String addres;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public String getAddres() {
+		return addres;
+	}
+	public void setAddres(String addres) {
+		this.addres = addres;
+	}
+	
+	
+}
