211db6f8d0a6804584ab04a7673c5a616b1abd04
swethanaagu <vishwakarmanagu@gmail.com>
2019-12-18 15:41:18 +0530

    remove unneccessory code

--- MySchool/src/com/vk/Student.java
+++ MySchool/src/com/vk/Student.java
@@ -1,47 +1,40 @@
 package com.vk;
 
 public class Student {
-		
+
 	private String name;
 	private int age;
 	private String address;
-	
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public int getAge() {
 		return age;
 	}
+
 	public void setAge(int age) {
 		this.age = age;
 	}
+
 	public String getAddres() {
 		return address;
 	}
+
 	public void setAddres(String address) {
 		this.address = address;
-<<<<<<< HEAD
-=======
 	}
-	
-	public Student(String name, int age, String address){
-		this.name = name;
-		this.age = age;
-		this.address = address;
->>>>>>> branch 'master' of https://github.com/nagaraju275521/MySchool.git
-	}
-	
-	
-		public static void main(String ar[]){
-			
-			for(int i=0; i<10; i++){
-				System.out.println("numbers: "+i);
-			}
+
+	public static void main(String ar[]) {
+
+		for (int i = 0; i < 10; i++) {
+			System.out.println("numbers: " + i);
 		}
-	
-	
-	
+	}
+
 }
